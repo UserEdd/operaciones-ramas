cd6663a2c6049e0b7a5fa54907b9c6ea3773e73b
UserEdd <eduardodominguez@laselva.edu.mx>
2024-06-07 00:20:57 -0600

    Operacion multiplicación

--- index.py
+++ index.py
@@ -17,3 +17,12 @@ resta = numero1 - numero2
 print("El resultado es: ", suma)
 
 
+# Operación multiplicación (rama feature)
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+multiplicacion = numero1 * numero2
+print("El resultado es: ", multiplicacion)
+
+
