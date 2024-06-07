bc8805af44c5183fcac95f433dca3507ca2028ff
UserEdd <eduardodominguez@laselva.edu.mx>
2024-06-07 00:11:09 -0600

    Operacion resta

---
+++ index.py
@@ -0,0 +1,19 @@
+
+suma = 0
+
+# Operación suma (rama master)
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+print("El resultado es: ", suma)
+
+
+# Operación resta (rama develop)
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+resta = numero1 - numero2
+print("El resultado es: ", suma)
+
+
