83b937fd6b325132abecdde92987cd58b71286ef
UserEdd <eduardodominguez@laselva.edu.mx>
2024-06-07 00:03:41 -0600

    Operación suma

---
+++ README.md
@@ -0,0 +1,9 @@
+Crear un programa que haga suma, resta, multiplicacion, divicion de dos numeros
+
+Rama master = suma
+Rama develop = resta
+Rama feature = multiplicacion
+Rama release = divicion
+
+En Oneflow
+
---
+++ index.py
@@ -0,0 +1,11 @@
+
+suma = 0
+
+# Operación suma (rama master)
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+print("El resultado es: ", suma)
+
+
